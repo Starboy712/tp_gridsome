f2d8cc2e9fafaaae6459115046886baae4f38cf9
Starboy712 <maxence.guichard@edu.univ-fcompte.fr>
2021-11-12 15:31:51 +0100

    afficher les projets dans la page "Projets.vue"

--- src/pages/Projet.vue
+++ src/pages/Projet.vue
@@ -1,14 +1,32 @@
 <template>
   <Layout>
-    <h1>Liste des projets</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>Liste des projets </h1>
+    <ul>
+      <li v-for="{node:projet} of $page.allProjet.edges" :key="projet.Id">
+        <g-link :to="projet.path">{{ projet.title }}</g-link>
+      </li>
+    </ul>
   </Layout>
 </template>
+
+<page-query>
+query {
+  allProjet {
+    edges {
+      node {
+      id
+      path
+      title
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Liste des projets'
+    title: 'About us'
   }
 }
 </script>
