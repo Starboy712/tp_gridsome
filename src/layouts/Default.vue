691818aff9cf0a20ba06f97f9f3f4350c037bde2
Starboy712 <maxence.guichard@edu.univ-fcompte.fr>
2021-11-12 14:44:23 +0100

    ajout page "liste projets" (src/pages/Projets.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,7 +6,9 @@
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/Projets">Projets</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
+
       </nav>
     </header>
     <slot/>
